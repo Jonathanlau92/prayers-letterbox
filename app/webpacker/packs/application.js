2c64375aa8e17f144f4e04a09e023dad565378ec
Jonathanlau92 <jonathan@excide.co>
2020-04-07 22:46:18 +0800

    Add navbar header and logo

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -16,5 +16,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+// Import images
+const images = require.context('../src/images', true)
+
 // Import bootstrap
 import 'bootstrap/dist/js/bootstrap';
