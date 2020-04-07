717f297e8ff653cd047c6614516f83ca6774a0e0
Jonathanlau92 <jonathan@excide.co>
2020-04-07 20:27:17 +0800

    Add routes

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,2 +1,15 @@
 class PrayersController < ApplicationController
+  def index
+      @prayers = Prayer.all
+  end
+
+  def new
+
+  end
+
+
+  private
+  def prayer_params
+    params.require(:prayer).permit(:request, :user_particulars, :is_deleted, :prayer_count)
+  end
 end
