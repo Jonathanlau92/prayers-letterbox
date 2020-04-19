3b8d53f1d2fc05acb9d7520cfa92631047afaa90
Jonathanlau92 <jonathan@excide.co>
2020-04-19 18:15:57 +0800

    Add spam button and method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :prayers
   post 'send_prayer_email', to: 'prayers#send_prayer_email', as: :send_prayer_email
+  post 'report-spam', to: 'prayers#report_spam', as: :report_spam
 
   get 'about-us', to: 'pages#about_us', as: :about_us
 
