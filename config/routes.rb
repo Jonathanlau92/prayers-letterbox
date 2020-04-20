fe1e1b20ae2541cac5112514bb74f8556b477063
timothy <timhou@outlook.com>
2020-04-20 23:51:54 +0800

    fix administrate bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :prayers
+
+      root to: "prayers#index"
+    end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :prayers
