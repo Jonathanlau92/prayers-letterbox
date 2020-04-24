63f6fca451f63b880f085933d12a13a4cee4b263
Jonathanlau92 <jonathan@excide.co>
2020-04-24 22:57:56 +0800

    Add seed file to generate admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+qingfeng = User.create(email: 'jonathan@gmail.com', password: 'password')
+tim = User.create(email: 'timhou@gmail.com', password: 'password')
