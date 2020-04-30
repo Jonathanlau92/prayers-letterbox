0423bb983955a21271f2c586c2c3bd744cfb7e80
Jonathanlau92 <jonathan@excide.co>
2020-04-30 23:10:10 +0800

    Add devise confirmation email

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
     root to: "prayers#index"
   end
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :prayers, except: :show
   get '/prayer-request', to: 'prayers#prayer_request', as: :prayer_request
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_092018) do
+ActiveRecord::Schema.define(version: 2020_04_30_145730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 2020_04_19_092018) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
