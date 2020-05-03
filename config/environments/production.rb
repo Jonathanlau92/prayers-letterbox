70e2ae67e7a05a6d3aa85f0bff139dcb49d1e8f4
Jonathanlau92 <jonathan@excide.co>
2020-05-03 20:21:56 +0800

    Add host for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-
+  config.action_mailer.default_url_options = { :host => ENV['HOST'], :protocol => 'https' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
