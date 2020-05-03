4efa8c4c9cf10b1d75653b69e5041ac5afc9bbeb
Jonathanlau92 <jonathan@excide.co>
2020-05-03 11:46:25 +0800

    Re-migrate current_life_stage to text format

--- db/migrate/20200503034359_add_extra_columns_to_users.rb
+++ db/migrate/20200503034359_add_extra_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddExtraColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :church, :string
-    add_column :users, :current_stage, :string
+    add_column :users, :current_life_stage, :text
     add_column :users, :name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_033735) do
+ActiveRecord::Schema.define(version: 2020_05_03_034359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_05_03_033735) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "church"
-    t.string "current_stage"
+    t.text "current_life_stage"
     t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
