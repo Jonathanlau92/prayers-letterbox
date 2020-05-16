f0ca1df3ed14afbe070a7ab0217778c8de877259
Jonathanlau92 <jonathan@excide.co>
2020-05-16 20:29:36 +0800

    Modify code for omniauth facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,43 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
   has_one_attached :profile_image
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session[“devise.facebook_data”] && session[“devise.facebook_data”][“extra”][“raw_info”]
-        user.email = data[“email”] if user.email.blank?
-      end
-    end
-  end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      user.skip_confirmation!
     end
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  # def self.new_with_session(params, session)
+  #   super.tap do |user|
+  #     if data = session[“devise.facebook_data”] && session[“devise.facebook_data”][“extra”][“raw_info”]
+  #       user.email = data[“email”] if user.email.blank?
+  #     end
+  #   end
+  # end
+
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0,20]
+  #     user.name = auth.info.name # assuming the user model has a name
+  #     user.image = auth.info.image # assuming the user model has an image
+  #   end
+  # end
 end
