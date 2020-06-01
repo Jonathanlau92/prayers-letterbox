d1928e1ab065da74cf0804d57b7d99d43d9253e3
timothy <timhou@outlook.com>
2020-06-01 20:55:08 +0800

    able to submit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,25 @@
 class CommentsController < ApplicationController
-    before_action :authenticate_user!
-    
-    def create
-        @comment = Comment.new(comment_params)
-        @comment.is_deleted = false
-       
-        if @comment.save!
-          head :ok
-        else
-          head :internal_server_error
-        end
+  before_action :authenticate_user!
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.is_deleted = false
+   
+    respond_to do |format|
+      if @comment.save!
+        format.html { redirect_to @comment.prayer, notice: 'Comment was successfully created.' }
+        format.js   { }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        head :internal_server_error
+      end
     end
-
-    private
-    def comment_params
-      params.require(:comment).permit(:content, :prayer_id, :user_id)
-    end
+  end
+  
+  def show
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :prayer_id, :user_id)
+  end
 end
