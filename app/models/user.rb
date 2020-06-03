6ac1e9849bb9d0fc906d8e31df2a527ff3903be9
Jonathanlau92 <jonathan@excide.co>
2020-06-03 21:58:18 +0800

    Add more users in seed file so that we can test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   rolify
+  has_friendship
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-qingfeng = User.create(email: 'jonathan@gmail.com', password: 'password')
+qingfeng = User.create(email: 'jonathan@gmail.com', password: 'password', name: 'qf')
 qingfeng.add_role(:user)
 qingfeng.add_role(:admin)
-tim = User.create(email: 'timhou@gmail.com', password: 'password')
+tim = User.create(email: 'timhou@gmail.com', password: 'password', name: 'tim')
 tim.add_role(:user)
 tim.add_role(:admin)
+
+# Add more user to test
+kenny = User.create(email: 'kenny@gmail.com', password: 'password', name: 'kenny')
+tinghan = User.create(email: 'daniel@gmail.com', password: 'password', name: 'tinghan')
+daniel = User.create(email: 'tinghan@gmail.com', password: 'password', name: 'daniel')
+john = User.create(email: 'john@gmail.com', password: 'password', name: 'john')
+brandon = User.create(email: 'brand@gmail.com', password: 'password', name: 'brandon')
+nancy = User.create(email: 'nancy@gmail.com', password: 'password', name: 'nancy')
