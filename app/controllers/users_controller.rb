30fa3017ac75381753b082ea8f248d9dd85d8329
Jonathanlau92 <jonathan@excide.co>
2020-06-03 21:58:33 +0800

    Add friend list so that user can follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user
+  before_action :set_user, except: :index
+
+  def index
+    @users = User.all
+  end
 
   def edit
     
