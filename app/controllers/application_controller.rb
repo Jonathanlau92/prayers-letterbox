ad1e8c918017ee5b19c49e43bc79b896bd9da706
Jonathanlau92 <jonathan@excide.co>
2020-06-07 00:15:08 +0800

    Add CSRF protection for ajax sending

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  protected
 
-    protected
-
-    def after_sign_in_path_for(resource)
-        if resource.is_a?(User) && resource.roles.blank?
-            resource.add_role(:user)
-        end
-        super
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User) && resource.roles.blank?
+      resource.add_role(:user)
     end
+    super
+  end
 end
