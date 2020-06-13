124da17b5e5d34c9a157cc95558ef2a4b05170d1
timothy <timhou@outlook.com>
2020-06-13 20:30:41 +0800

    add selectize for UI

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("selectize")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -28,6 +29,9 @@ import '../src/javascripts/direct_upload';
 document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
+    $("#prayer_categories").selectize({
+        maxItems: 10
+    });
 })
 
 // Import font-awesome icons
