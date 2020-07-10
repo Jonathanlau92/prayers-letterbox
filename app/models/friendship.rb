01737ca31e3f72ce019fd88f52a8cf2df42d5f8b
Jonathanlau92 <jonathan@excide.co>
2020-07-11 00:29:18 +0800

    Display pending friend requests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,12 +5,16 @@ class Friendship < ApplicationRecord
   # acts_as_notifiable :users,
   #   # Notification targets as :targets is a necessary option
   #   # Set to notify to author and users commented to the article, except comment owner self
-  #   targets: ->(comment, key) {
-  #     ([comment.article.user] + comment.article.commented_users.to_a - [comment.user]).uniq
-  #   },
+  #   targets: :custom_notification_targets,
   #   # Path to move when the notification is opened by the target user
   #   # This is an optional configuration since activity_notification uses polymorphic_path as default
   #   notifiable_path: :article_notifiable_path
+
+  # def custom_notification_targets(key)
+  #   if key == 'friendship.get_friend_request'
+  #     [self.user]
+  #   end
+  # end
 
   # def article_notifiable_path
   #   article_path(article)
