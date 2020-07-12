8f53f1e1a5d3662f58f509c63d7d754bc683b556
Jonathanlau92 <jonathan@excide.co>
2020-07-12 13:32:33 +0800

    Allow accept request feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, except: [:index, :add_friends]
+  before_action :set_user, except: [:index, :add_friends, :accept_request]
 
   def index
     @users = User.all
@@ -26,6 +26,15 @@ class UsersController < ApplicationController
     current_user.friend_request(@friend)
     respond_to do |format|
       format.json { render json: { friend_name: @friend }, status: :ok, friend: @friend.name }
+    end
+  end
+
+  def accept_request
+    @friend = User.find_by(id: params[:friend_id])
+    if current_user.accept_request(@friend)
+      redirect_to user_path(current_user.id), notice: 'Accept friend request!'
+    else
+      redirect_to root_path, alert: 'Accepting friend request has failed. Please contact admin'
     end
   end
 
