00953828afb969dbc39ee0513db2877f9fd4f0e7
timothy <timhou@outlook.com>
2020-08-09 16:41:27 +0800

    add status to prayer

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,4 +1,5 @@
 class Prayer < ApplicationRecord
+  enum status: { active: 0, archived: 1, completed: 2 }
   belongs_to :user, optional: true
   has_many :comments
   has_many :categories, dependent: :destroy
